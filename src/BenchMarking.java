f39ac46bc27a72ca8040f6b930fbd09f7c9f5503
Alfonso Auquilla <alfonsoauquilla180@gmail.com>
2026-05-04 08:57:22 -0500

    segundo comit

---
+++ src/BenchMarking.java
@@ -0,0 +1,27 @@
+import models.Results;
+import java.util.concurrent.Callable;
+
+
+public class BenchMarking {
+
+  public static Results medirTiempo(Callable<Void> funcion , String name, int sample){
+    try {
+        long inicio = System.nanoTime();
+        funcion.call();
+        long fin = System.nanoTime();
+        double duracion = fin - inicio;
+
+        Results resultados = new Results(duracion, name, sample);
+        return resultados;
+        
+    } catch (Exception e) {
+
+        throw new RuntimeException("Error en la funcion");
+        
+    }
+
+    
+
+  }
+    
+}
